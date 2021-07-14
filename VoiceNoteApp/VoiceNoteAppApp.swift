f68b1f763f0a0beee2e94d3f3074efe39a7ab57c
Deepak2909 <sdyadav200@gmail.com>
2021-07-14 14:09:12 +0530

    Initial Commit

---
+++ VoiceNoteApp/VoiceNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceNoteAppApp.swift
+//  VoiceNoteApp
+//
+//  Created by Deepak on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct VoiceNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
